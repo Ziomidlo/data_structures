ca372fcb8c8dc496779b9b94b07e4be4ad886dd6
ziomidlo <kziomek33@gmail.com>
2022-11-16 16:43:44 +0100

    implement remove function to tree alghorithm

--- Tree.py
+++ Tree.py
@@ -34,14 +34,12 @@ class Tree:
             return False
 
     def get_size(self):
-        size = 1
-        if self.data is not None:
-            if self.left is not None:
-                size += 1
-                return self.left.get_size()
-            if self.right is not None:
-                size += 1
-                return self.right.get_size()
+        if self.left is not None and self.right is not None:
+            return 1 + self.left.get_size() + self.right.get_size()
+        elif self.left:
+            return 1 + self.left.get_size()
+        elif self.right:
+            return 1 + self.right.get_size()
         else:
             return 1
 
@@ -60,6 +58,102 @@ class Tree:
             print(self.data, end=' ')
             if self.right is not None:
                 self.right.inorder_traversal()
+
+    def remove(self, data):
+
+        #empty tree
+        if self.data is None:
+            return False
+        
+        #data is in root node
+        elif self.left is None and self.right is None:
+           self.data = None
+        elif self.left and self.right is None:
+            self.data = self.left 
+        elif self.right and self.left is None:
+            self.data = self.right
+        elif self.left and self.right:
+            delNodeParent = self.data
+            delNode = self.right
+            while delNode.left:
+                delNodeParent = delNode
+                delNode = delNode.left
+            
+            self.data = delNode.data
+            if delNode.right:
+                if delNodeParent.data > delNode.data:
+                    delNodeParent.left = delNode.right
+                elif delNodeParent.data < delNode.data:
+                    delNodeParent.right = delNode.right
+            else:
+                if delNode.data < delNodeParent.data:
+                    delNodeParent.left = None
+                else:
+                    delNodeParent.right = None
+
+
+        # find node to remove
+        parent = None
+        while self.data != data:
+            parent = self.data
+            if self.data > data:
+                self.data = self.left
+            elif self.data < data:
+                self.data = self.right
+
+        #data not found
+        if self.data is None or self.data != data:
+            return False
+        
+        #remove-node has no children
+        elif self.left is None and self.right is None:
+            if parent.data > data:
+                parent.left = None
+            else:
+                parent.right = None
+            return True
+
+        #remove-node has left child only
+        elif self.left and self.right is None:
+            if parent.data > data:
+                parent.left = self.left
+            else:
+                parent.right = self.right
+            return True
+        
+        #remove-node has right child only
+        elif self.right and self.left is None:
+            if parent.data > data:
+                parent.left = self.left
+            else:
+                parent.right = self.right
+            return True 
+        
+        #remove-node has left and right children
+        else: 
+            delNodeParent = self.data
+            delNode = self.data.right
+            while delNode.left:
+                delNodeParent = delNode
+                delNode = delNode.left
+            
+            self.data = delNode.data
+            if delNode.right:
+                if delNodeParent.data > delNode.data:
+                    delNodeParent.left = delNode.right
+                elif delNodeParent.data < delNode.data:
+                    delNodeParent.right = delNode.right
+                else:
+                    if delNode.data < delNodeParent.data:
+                        delNodeParent.left = None
+                    else:
+                        delNodeParent.right = None
+
+
+
+
+
+                
 
 
         
